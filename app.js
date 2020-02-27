bde406cc2435e4d8c7b54ed9e3f4442f990adf88
John Cummings <jcummings5@gmail.com>
2020-02-26 19:02:25 -0800

    Added https for production.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 // make bluebird default Promise
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
+const https = require('https');
+const fs = require('fs');
 const { port, env } = require('./config/vars');
 const logger = require('./config/logger');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
 
+
 // open mongoose connection
 mongoose.connect();
 
-// listen to requests
-app.listen(port, '0.0.0.0', () => logger.info(`server started on port ${port} (${env})`));
+if (env === 'production') {
+  https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'Ireland2018',
+  }, app).listen(port, '0.0.0.0', () => logger.info(`server started on port ${port} (${env})`));
+} else {
+  // listen to requests
+  app.listen(port, '0.0.0.0', () => logger.info(`server started on port ${port} (${env})`));
+}
 
 /**
 * Exports express
